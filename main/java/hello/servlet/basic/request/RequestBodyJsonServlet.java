fcc4d276dc4bc8fd6ebb3150f69ddcde3a312cbd
layerdst <bccfcc@hanmail.net>
2022-07-04 23:49:51 +0900

    API

--- main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
+++ main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -1,5 +1,7 @@
 package hello.servlet.basic.request;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
 import org.springframework.util.StreamUtils;
 
 import javax.servlet.ServletException;
@@ -11,8 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-@WebServlet(name="requestBodyStringServlet" , urlPatterns = "/request-body-string")
-public class RequestBodyStringServlet extends HttpServlet {
+@WebServlet(name="requestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -21,6 +25,10 @@ public class RequestBodyStringServlet extends HttpServlet {
 
         System.out.println("messageBody : " + messageBody);
 
-        resp.getWriter().write("ok");
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+
+        System.out.println(helloData.getUsername());
+        System.out.println(helloData.getAge());
+
     }
 }
